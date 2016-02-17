1e8c14b75dc4229bbe5a2ebd20dcfebb3e5890ce
Mars Saxman <mars@redecho.org>
2016-02-17 08:32:49 -0800

    sample program testing parser round-trip

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,13 @@
 // IS" WITH NO EXPRESS OR IMPLIED WARRANTY.
 
 #include <iostream>
+#include <fstream>
 #include "lexer.h"
 #include "parser.h"
 #include "errors.h"
 #include "printast.h"
+
+#include <unistd.h>
 
 struct receiver: public ast::delegate {
 	virtual void ast_process(ast::ptr &&n) override {
@@ -21,14 +24,33 @@ struct receiver: public ast::delegate {
 	}
 };
 
+static int run(std::istream &i) {
+	errors e;
+	receiver o;
+	parser p(o, e);
+	lexer l(p, e);
+	l.read_file(i);
+}
+
 int main(int argc, const char *argv[]) {
-	std::cout << "$> ";
-	for (std::string line; std::getline(std::cin, line);) {
-		errors e;
-		receiver o;
-		parser p(o, e);
-		lexer l(p, e);
-		l.read_line(line);
+	if (argc <= 1 && isatty(fileno(stdin))) {
+		std::cout << "$> ";
+		for (std::string line; std::getline(std::cin, line);) {
+			errors e;
+			receiver o;
+			parser p(o, e);
+			lexer l(p, e);
+			l.read_line(line);
+		}
+		return EXIT_SUCCESS;
+	}
+	if (argc <= 1) {
+		return run(std::cin);
+	}
+	for (int i = 1; i < argc; ++i) {
+		std::ifstream file(argv[i]);
+		int ret = run(file);
+		if (ret) return ret;
 	}
 	return EXIT_SUCCESS;
 }
